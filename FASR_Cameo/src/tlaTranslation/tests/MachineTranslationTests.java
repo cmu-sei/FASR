39a2dec1f8c6e22d1b7e043c4ba28de104386477
Sam Procter <sprocter@sei.cmu.edu>
2025-10-06 14:54:05 -0400

    Begin expansion of TLA generation test class

--- FASR_Cameo/src/tlaTranslation/tests/MachineTranslationTests.java
+++ FASR_Cameo/src/tlaTranslation/tests/MachineTranslationTests.java
@@ -2,31 +2,53 @@ package tlaTranslation.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.eclipse.emf.common.util.BasicEList;
-import org.eclipse.emf.common.util.EList;
-import org.eclipse.uml2.uml.Behavior;
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
 import tlaTranslation.TLATranslator;
 import umlTraverse.TraverseModel;
 
 class MachineTranslationTests {
 	
-	@Before
-	void setup() {
+	protected String tla = "UNSET";
+	
+	protected String getMachineSpec(String path) {
+		TLATranslator translator = new TLATranslator(new TraverseModel(path));
 		
+		return translator.createMachineSpec();
 	}
-
-	@Test
-	void test() {
-		System.out.println("Starting tests!");
-		TraverseModel tm = new TraverseModel("Diagrams/WaterTank.uml");
-		EList<BasicEList<Behavior>> diagrams = tm.getDiagrams(null);
-		TLATranslator translator = new TLATranslator(tm);
-		
-		translator.createMachineSpec();
-		fail("Not yet implemented");
+	
+	@Nested
+	@TestInstance(Lifecycle.PER_CLASS)
+	public class WaterTankTests {
+		@BeforeAll
+		void setup() {
+			tla = getMachineSpec("Diagrams/WaterTank/WaterTank.uml");
+		}
+	
+		@Test
+		void test() {
+			System.out.println(tla);
+			fail("Not yet implemented");
+		}
+	}
+	
+	@Nested
+	@TestInstance(Lifecycle.PER_CLASS)
+	public class BSCUTests {
+		@BeforeAll
+		void setup() {
+			tla = getMachineSpec("Diagrams/BSCU/BSCU.uml");
+		}
+	
+		@Test
+		void test() {
+			System.out.println(tla);
+			fail("Not yet implemented");
+		}
 	}
 
 }
